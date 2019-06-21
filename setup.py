0332992900a06806032f335a9e4429d5bc6ef4b1
Thanaphong Phongpreecha <tp.joe@hotmail.com>
2019-06-21 16:37:15 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def read_long_description():
 
 
 setup(
-    name="sherlockml-boltzmannclean",
+    name="sherlockml-updated-boltzmannclean",
     version="0.1.2",
-    url="https://github.com/ASIDataScience/sherlockml-boltzmannclean",
-    author="ASI Data Science",
+    url="https://github.com/tpjoe/sherlockml-updated-boltzmannclean",
+    author="ASI Data Science (original)",
     author_email="opensource@asidatascience.com",
     description="Fill missing values in DataFrames with Restricted Boltzmann Machines",
     license="Apache 2.0",
     long_description=read_long_description(),
-    py_modules=["boltzmannclean"],
+    py_modules=["boltzmannclean_updated"],
     install_requires=["pandas", "numpy", "scipy", "scikit-learn"],
 )
